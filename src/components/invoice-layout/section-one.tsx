27078ffbeff9d7b78cfffae3d89c284c7408589d
Olawalecodes <iwaloyeo@gmail.com>
2024-10-06 17:13:26 +0100

    updates: completed invoice  section one cards

--- src/components/invoice-layout/section-one.tsx
+++ src/components/invoice-layout/section-one.tsx
@@ -1,26 +1,71 @@
+import { currencySymbol } from "@/lib/constants";
 import { Card, CardContent } from "../ui/card";
 
 const SectionOne = () => {
   return (
-    <section className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-10">
-      <Card className="h-[204px]">
-        <CardContent>
-          <p>Card Content</p>
+    <section className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8 mt-10">
+      <Card className="h-[180px]">
+        <CardContent className="py-8 px-10 flex flex-col gap-2.5">
+          <img src="/category.svg" alt="category" className="w-10 h-10" />
+          <div className="flex gap-2 items-center">
+            <p className="uppercase text-xs font-normal text-grey">
+              Total paid
+            </p>
+            <p className="px-3 py-1 bg-lightGreen text-darkGrey text-sm font-medium tracking-[-0.43px] rounded-3xl">
+              1,289
+            </p>
+          </div>
+          <p className="text-[1.623rem] tracking-wide font-medium">
+            {currencySymbol}4,120,102.<span className="text-sm">76</span>
+          </p>
         </CardContent>
       </Card>
-      <Card className="h-[204px]">
-        <CardContent>
-          <p>Card Content</p>
+      <Card className="h-[180px]">
+        <CardContent className="py-8 px-10 flex flex-col gap-2.5">
+          <img src="/category.svg" alt="category" className="w-10 h-10" />
+          <div className="flex gap-2 items-center">
+            <p className="uppercase text-xs font-normal text-grey">
+              Total Overdue
+            </p>
+            <p className="px-3 py-1 bg-lightRed text-darkGrey text-sm font-medium tracking-[-0.43px] rounded-3xl">
+              13
+            </p>
+          </div>
+          <p className="text-[1.623rem] tracking-wide font-medium">
+            {currencySymbol}4,120,102.<span className="text-sm">76</span>
+          </p>
         </CardContent>
       </Card>
-      <Card className="h-[204px]">
-        <CardContent>
-          <p>Card Content</p>
+      <Card className="h-[180px]">
+        <CardContent className="py-8 px-10 flex flex-col gap-2.5">
+          <img src="/category.svg" alt="category" className="w-10 h-10" />
+          <div className="flex gap-2 items-center">
+            <p className="uppercase text-xs font-normal text-grey">
+              Total Draft
+            </p>
+            <p className="px-3 py-1 bg-lightGrey text-darkGrey text-sm font-medium tracking-[-0.43px] rounded-3xl">
+              08
+            </p>
+          </div>
+          <p className="text-[1.623rem] tracking-wide font-medium">
+            {currencySymbol}4,120,102.<span className="text-sm">76</span>
+          </p>
         </CardContent>
       </Card>
-      <Card className="h-[204px]">
-        <CardContent>
-          <p>Card Content</p>
+      <Card className="h-[180px]">
+        <CardContent className="py-8 px-10 flex flex-col gap-2.5">
+          <img src="/category.svg" alt="category" className="w-10 h-10" />
+          <div className="flex gap-2 items-center">
+            <p className="uppercase text-xs font-normal text-grey">
+              Total Unpaid
+            </p>
+            <p className="px-3 py-1 bg-lightYellow text-darkGrey text-sm font-medium tracking-[-0.43px] rounded-3xl">
+              06
+            </p>
+          </div>
+          <p className="text-[1.623rem] tracking-wide font-medium">
+            {currencySymbol}4,120,102.<span className="text-sm">76</span>
+          </p>
         </CardContent>
       </Card>
     </section>
